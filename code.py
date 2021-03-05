f7835b88930f6a0af5206a618362e96b8d256e4a
don <novaspirit@novaspirit.com>
2021-03-05 09:00:55 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+import time
+import usb_hid
+from adafruit_hid.mouse import Mouse
+import board
+import digitalio
+
+mouse = Mouse(usb_hid.devices)
+
+led = digitalio.DigitalInOut(board.GP25)
+led.direction = digitalio.Direction.OUTPUT
+
+led.value = False
+time.sleep(5)
+
+while True:
+	led.value = True
+	mouse.move(x=100)
+	led.value = False
+	time.sleep(0.5)
+	led.value = True
+	mouse.move(x=100)
+	led.value = False
+	time.sleep(0.5)
